c2eeebadc8c3296c4e382c40db6bda11e0f37da5
jayden jardine <jayster7101@gmail.com>
2022-10-11 09:17:14 -0700

    better program flow main

--- main.cpp
+++ main.cpp
@@ -15,24 +15,24 @@ int main()
         int const size = 50000;
         int arry[size];
         random_gen(size, arry, list);
-        double lltime, atime;
-        std::cout << "\nArray and linked lists of size 50,000 created filled "
-                     "the same random data (integers)\n\n";
+        double lltime, atime = 0;
+        std::cout << "\nArray and linked lists of size 50,000 created and have been filled with random data (integers)\n\n";
 
-        std::cout << "The first 50 contents in the lists and array are:\n\n";
+        std::cout << "The first 50 items in the unsorted list and array are:\n\n";
         print50(list);
 
-        std::cout << "Now sorting Linked List...\n";
+        std::cout << "Sorting Linked List...\n";
+        lltime = insertion_LL(list);
         lltime = insertion_LL(list);
 
         std::cout << "Now sorting Array...\n\n";
         atime = insertion_A(size, arry);
+        atime = insertion_A(size, arry);
 
         show_and_explain(size, list, arry, lltime, atime);
 
-        std::cout
-            << "The first 50 contents in the SORTED lists and array are:\n\n";
-        print_list(list);
+        std::cout << "The first 50 items in the SORTED lists and array are:\n\n";
+         print50(list);
 
         std::cout << "Do you want to continue? (Yes/No)\n";
         run = enter();
