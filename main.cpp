0f94a0702fd187c3564dfc01ca9d5a2d550a98ed
Varun Narravula <varun@cvte.org>
2022-10-11 09:55:33 -0700

    Differentiate unsorted vs sorted runs

--- main.cpp
+++ main.cpp
@@ -16,27 +16,37 @@ int main()
         int arry[size];
         random_gen(size, arry, list);
         double lltime, atime = 0;
-        std::cout << std::endl << "Array and linked lists of size 50,000 created and have been filled with random data (integers)" << std::endl << std::endl;
+        std::cout << std::endl
+                  << "Array and linked lists of size 50,000 created and have "
+                     "been filled with random data (integers)"
+                  << std::endl
+                  << std::endl;
 
-        std::cout << "The first 50 items in the unsorted list and array are:" << std::endl << std::endl;
+        std::cout << "The first 50 items in the unsorted list and array are:"
+                  << std::endl
+                  << std::endl;
         print50(list);
 
-        std::cout << "Sorting Linked List..." << std::endl;
+        std::cout << "Sorting unsorted Linked List..." << std::endl;
         lltime = insertion_LL(list);
-        lltime = insertion_LL(list);
-
-        std::cout << "Now sorting Array..." << std::endl << std::endl;
-        atime = insertion_A(size, arry);
+        std::cout << "Now sorting unsorted Array..." << std::endl << std::endl;
         atime = insertion_A(size, arry);
 
         show_and_explain(size, list, arry, lltime, atime);
 
-        std::cout << "The first 50 items in the SORTED lists and array are:" << std::endl << std::endl;
-         print50(list);
+        std::cout << "Sorting sorted Linked List..." << std::endl;
+        lltime = insertion_LL(list);
+        std::cout << "Now sorting sorted Array..." << std::endl << std::endl;
+        atime = insertion_A(size, arry);
+
+        show_and_explain(size, list, arry, lltime, atime);
+
+        std::cout << "The first 50 items in the SORTED lists and array are:"
+                  << std::endl
+                  << std::endl;
+        print50(list);
 
         std::cout << "Do you want to continue? (Yes/No)" << std::endl;
         run = enter();
     }
 }
-
-
