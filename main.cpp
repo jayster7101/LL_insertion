f1d5e6d5e640db3253998ff935008b69faeb5169
jayden jardine <jayster7101@gmail.com>
2022-10-11 09:40:23 -0700

    fixed ui, changed to endl

--- main.cpp
+++ main.cpp
@@ -16,37 +16,27 @@ int main()
         int arry[size];
         random_gen(size, arry, list);
         double lltime, atime = 0;
-        std::cout << "\nArray and linked lists of size 50,000 created and have been filled with random data (integers)\n\n";
+        std::cout << std::endl << "Array and linked lists of size 50,000 created and have been filled with random data (integers)" << std::endl << std::endl;
 
-        std::cout << "The first 50 items in the unsorted list and array are:\n\n";
+        std::cout << "The first 50 items in the unsorted list and array are:" << std::endl << std::endl;
         print50(list);
 
-        std::cout << "Sorting Linked List...\n";
+        std::cout << "Sorting Linked List..." << std::endl;
         lltime = insertion_LL(list);
         lltime = insertion_LL(list);
 
-        std::cout << "Now sorting Array...\n\n";
+        std::cout << "Now sorting Array..." << std::endl << std::endl;
         atime = insertion_A(size, arry);
         atime = insertion_A(size, arry);
 
         show_and_explain(size, list, arry, lltime, atime);
 
-        std::cout << "The first 50 items in the SORTED lists and array are:\n\n";
+        std::cout << "The first 50 items in the SORTED lists and array are:" << std::endl << std::endl;
          print50(list);
 
-        std::cout << "Do you want to continue? (Yes/No)\n";
+        std::cout << "Do you want to continue? (Yes/No)" << std::endl;
         run = enter();
     }
 }
 
-// prints list
-void print_list(const List* list)
-{
-    Node* trav = list->head;
-    while (trav != nullptr)
-    {
-        std::cout << trav->data << " ";
-        trav = trav->next;
-    }
-    std::cout << "\n";
-}
+
